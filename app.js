25b86dbcc2b59aa5c29d2dda3f7998add9b8c796
Soichi Hayashi <soichih@gmail.com>
2014-11-12 22:55:35 +0000

    removed console.log which was somehow inserted to result xml

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ app.get('/api', sampleController.apihome);
 var server = app.listen(3000, function () {
     var host = server.address().address
     var port = server.address().port
-    console.log('Example app listening at http://%s:%s', host, port)
-
+    console.debug('Example app listening at http://%s:%s', host, port)
 })
 
 module.exports = app;
